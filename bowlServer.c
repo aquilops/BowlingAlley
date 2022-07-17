a453c111bb38b950f38e11418f2f836b0a2634a5
Aquilops <105448215+aquilops@users.noreply.github.com>
2022-07-17 18:51:22 +0800

    Update bowlServer.c

--- bowlServer.c
+++ bowlServer.c
@@ -69,7 +69,8 @@ int main(int argc , char *argv[]) {
                         while(1){
                                  recv(new_socket, &p, sizeof(int),0);
                                 if(strcmp(buffer, ":exit") == 0){
-                                        printf("Disconnected from %s:%d\n", inet_ntoa(client>
+                                        printf("Disconnected from %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+
                                         break;
                                 }
                                 else{
@@ -89,12 +90,13 @@ int main(int argc , char *argv[]) {
                                         message = ("Your Total Price is: ");
                                         write(new_socket,message,strlen(message));
 
-                                        printf("Disconnected from %s:%d\n", inet_ntoa(client>
+                                        printf("Disconnected from %s:%d\n", inet_ntoa(client.sin__addr), ntohs(client.sin_port));
+
                                         break;
 										      
 }
  }
-}
+}}
 close(new_socket);
 
  return 0;
